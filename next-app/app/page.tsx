befaf8966c49f8703be2e1d2dd306cceda5d584e
ivancostabernardo <ivancbernardo@gmail.com>
2025-10-09 23:58:48 -0400

    Update page.tsx

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -7,8 +7,9 @@ export default function Home() {
 
   // Try both URLs - one of them should work after route configuration
   const API_URLS = [
-    'https://zhndd3sr1a.execute-api.us-east-2.amazonaws.com/default',
-    'https://zhndd3sr1a.execute-api.us-east-2.amazonaws.com/NewStage/'
+    'https://zhndd3sr1a.execute-api.us-east-2.amazonaws.com/NewStage/test-button-function-v2',
+    'https://zhndd3sr1a.execute-api.us-east-2.amazonaws.com/NewStage/test',
+    'https://zhndd3sr1a.execute-api.us-east-2.amazonaws.com/NewStage/
   ];
 
   const [currentUrlIndex, setCurrentUrlIndex] = useState(0);
